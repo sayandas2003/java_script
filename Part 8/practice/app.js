a725c8647869496d445d67e9c92557a5d90a2dd3
Sayan Das <121926159+sayandas2003@users.noreply.github.com>
2025-03-21 17:32:21 +0530

    part 8 completed

---
+++ Part 8/practice/app.js
@@ -0,0 +1,33 @@
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+// let ans = nums.filter((el)=>el%2==0);
+
+// function min(a, b, c, d) {
+//     console.log(arguments.length);
+//     console.log(arguments[0]);
+// }
+
+// function sum(arguments) {
+//     return arguments.reduce(function sum(sum,el) {
+//         return sum+el
+//     });
+// }
+
+
+// let names = ["tony", "bruce", "peter", "steve"];
+// let [winner, runnerup, ...others] = names;
+// console.log(winner, runnerup, others);
+
+
+// let arr = [1,2,3,4,5,6,7];
+// let sqarearr = arr.map((el)=>{
+//     return el*el;
+// });
+// let sum = sqarearr.reduce((acc,el)=>acc+el);
+// let avg = sum / arr.length;
+
+
+
+// let arr = ["sayan", "ranisha"];
+// console.log(arr.map((el)=>el.toUpperCase()));
+
+
